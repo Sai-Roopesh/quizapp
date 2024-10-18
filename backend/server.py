63948c74436dcbdfa635cdad2a35a27bc54def23
Sai Roopesh <sairoopesh21@gmail.com>
2024-10-19 01:09:15 +0530

    cors i give up man

--- backend/server.py
+++ backend/server.py
@@ -29,7 +29,7 @@ app = FastAPI()
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Hard-coded allowed origins
+    allow_origins=["https://quizapp-frontend-chi.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
